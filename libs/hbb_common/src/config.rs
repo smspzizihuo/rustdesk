5430381720c98ec2e59e61ecc85ec9c22ea29015
smspzizihuo <87960389+smspzizihuo@users.noreply.github.com>
2024-09-06 21:20:14 +0800

    config.rs

--- libs/hbb_common/src/config.rs
+++ libs/hbb_common/src/config.rs
@@ -100,8 +100,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const PUBLIC_RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["pizizhuo-morito.dns.army"];
+pub const PUBLIC_RS_PUB_KEY: &str = "NOCA5QZ7dKdvgtDEBBXo5TNhwDz57q0lyWpD0jlAGxQ=";
 
 pub const RS_PUB_KEY: &str = match option_env!("RS_PUB_KEY") {
     Some(key) if !key.is_empty() => key,
